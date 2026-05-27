dee0df51f0b06ccbbf4ca1fa3e3792cffd6a1953
junyeong <s25035@gsm.hs.kr>
2026-05-27 18:28:34 +0900

    feat: 역할 기준 사용자 조회 추가

--- src/main/java/com/example/kodyjobdam/user/UserRepository.java
+++ src/main/java/com/example/kodyjobdam/user/UserRepository.java
@@ -3,9 +3,13 @@ package com.example.kodyjobdam.user;
 import com.example.kodyjobdam.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    List<Integer> findByRole(UserRole role);
 }
